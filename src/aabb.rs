39b295188e07a0944d2a23c7492e53cd0bfafbdf
David Hinton <hintondavid11@gmail.com>
2020-06-26 22:43:40 -0400

    Use inverse direction in AABBs to speed up rendering.

--- src/aabb.rs
+++ src/aabb.rs
@@ -22,11 +22,12 @@ impl AABB {
 
     #[inline]
     pub fn hit(&self, r: &Ray, tmin: f32, tmax: f32) -> bool {
-        for i in 0..3 {
-            let inv_d = 1.0 / r.direction().get(i);
-            let mut t0 = (self.min.get(i) - r.origin().get(i)) * inv_d;
+        for i in 0..3 {	
+            let inv_d = r.inv_direction().get(i);
+			let mut t0 = (self.min.get(i) - r.origin().get(i)) * inv_d;
             let mut t1 = (self.max.get(i) - r.origin().get(i)) * inv_d;
-            if inv_d < 0.0 {
+            
+			if r.dir_sign()[i] {
                 std::mem::swap(&mut t0, &mut t1);
             }
             let tmin = if t0 > tmin { t0 } else { tmin };
