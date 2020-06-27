3467a9888cd23c1e1a5d70cbd491363c01fbaa5d
David Hinton <hintondavid11@gmail.com>
2020-06-26 22:28:28 -0400

    Rename fields in Ray to better suit their uses, as well as precompute some values in it to speed up rendering.

--- src/ray.rs
+++ src/ray.rs
@@ -2,24 +2,42 @@ use super::vec3::*;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Ray {
-    a: Vec3,
-    b: Vec3,
+    origin: Vec3,
+    direction: Vec3,
+	inv_direction: Vec3, // stores the inverse of the direction, for speed.
+	dir_sign: [bool; 3],	//stores the sign of the direction, for speed.
 }
 
 impl Ray {
-    pub fn new(a: Vec3, b: Vec3) -> Ray {
-        Ray { a, b }
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        let inv_direction = Vec3::new(
+					1.0/direction.x(), 
+					1.0/direction.y(), 
+					1.0/direction.z());
+		let dir_sign = [
+					direction.x() < 0.0, 
+					direction.y() < 0.0, 
+					direction.z() < 0.0];
+		Ray { origin, direction, inv_direction, dir_sign }
     }
 
     pub fn origin(&self) -> Vec3 {
-        self.a
+        self.origin
     }
 
     pub fn direction(&self) -> Vec3 {
-        self.b
+        self.direction
     }
 
+    pub fn inv_direction(&self) -> Vec3 {
+        self.inv_direction
+    }
+	
+	pub fn dir_sign(&self) -> [bool; 3] {
+		self.dir_sign
+	}
+
     pub fn point_at_parameter(&self, t: f32) -> Vec3 {
-        self.a + self.b * t
+        self.origin + self.direction * t
     }
 }
