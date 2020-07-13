fd5f2eb51679bdf54397e16b5b4860482d2947fd
David Hinton <hintondavid11@gmail.com>
2020-07-13 11:03:23 -0400

    Add a set method to Vec3.

--- src/vec3.rs
+++ src/vec3.rs
@@ -41,6 +41,21 @@ impl Vec3 {
         );
     }
 
+    pub fn set(&mut self, index: usize, value: f32) {
+        if index == 0 {
+            self.x = value;
+        } else if index == 1 {
+            self.y = value;
+        } else if index == 2 {
+            self.z = value;
+        } else {
+            panic!(
+                "Attempted to access Vec3 out of bounds with index: {} !",
+                index
+            );
+        }
+    }
+
     pub fn length(&self) -> f32 {
         (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
     }
@@ -136,5 +151,17 @@ impl std::ops::Div<f32> for Vec3 {
 
     fn div(self, other: f32) -> Vec3 {
         Vec3::new(self.x / other, self.y / other, self.z / other)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn test_set() {
+        let mut v = Vec3::new(1.0, 2.0, 3.0);
+        v.set(0, 4.0);
+        assert_eq!(v.x(), 4.0);
     }
 }
